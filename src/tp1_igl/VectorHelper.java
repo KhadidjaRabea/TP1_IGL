42dc594e00af22b6a1aa2166027580e8d48f39dc
Khadidja <Khadidja@DESKTOP-1U2D94O>
2017-10-16 21:52:09 +0200

    VectorHelper ( méthode 2,3,4 + la méthode Affiche + la méthode length )

---
+++ src/tp1_igl/VectorHelper.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp1_igl;
+
+/**
+ *
+ * @author Khadidja
+ */
+public class VectorHelper {
+    
+    
+       
+    
+   public void AffichVecteur(int vect[])
+    {   System.out.print("|");
+        for (int i: vect)
+        {
+            System.out.print(i+"|");
+        }
+    }
+   
+ 
+   public int length(int Vect[])
+   {   int l=0; 
+    for (int i: Vect)
+   { 
+       l++;
+   } 
+      return l;
+   }  
+   
+ 
+   public void MaxMin (int Vect[]){
+      
+       int max=Vect[0];
+       int min=Vect[0];
+       
+       for( int i: Vect)
+       {
+           if (i> max) max=i;
+           if (i<min) min=i;
+       }
+       System.out.println("\n");
+       System.out.println("Max= "+max+", Min="+min+" \n");
+   }
+   
+
+   public void inverser(int Vect [],int l)
+   {
+       int inf,sup,x;
+       inf=0; sup=l-1;
+       
+       while(inf<=sup)
+       {
+           x=Vect[sup];
+           Vect[sup]=Vect[inf];
+           Vect[inf]=x;
+           
+           inf++; sup--;
+       }
+   }
+   
+   
+    public void Somme2Vect(int V1[], int V2[], int l1,int l2) throws diffrenceInLengthException{
+       
+       if(l1!=l2)
+           throw new diffrenceInLengthException(" ERROR! les deux vecteurs n'ont pas la meme longueur");
+       else 
+       {
+           for ( int i=0;i<l1;i++)
+           {
+               V1[i]+=V2[i];
+           }
+       }
+           
+   }
+   
+  
+
+    
+}
---
+++ src/tp1_igl/diffrenceInLengthException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp1_igl;
+
+/**
+ *
+ * @author Khadidja
+ */
+public class diffrenceInLengthException extends Exception {
+  
+        public diffrenceInLengthException(String message){
+        super(message);
+    }
+}
