ab7f2b6ba449e5d041eef3802cb04b49f63fc386
Khadidja <Khadidja@DESKTOP-1U2D94O>
2017-10-24 21:15:03 +0200

    version_finale.1

--- src/tp1_igl/VectorHelper.java
+++ src/tp1_igl/VectorHelper.java
@@ -13,47 +13,65 @@ package tp1_igl;
 
 public class VectorHelper {
     
+   private int []Vect;
+   private int min;
+   private int max;
+   private int l;
+
    
     
-    
-    /** Afficher les éléments du vecteur vect
+    /**
      *
      * @param vect un vecteur d'entiers 
+     * @param l la taille de la veteur  
      */
-    
-    public void VectorHelper (int vect []){
+     VectorHelper (int vect []){
        
+        Vect=vect;
+        this.l=Vect.length;
     }
-    public void AffichVecteur(int vect[])
-    {   System.out.print("|");
-        for (int i: vect)
-        {
-            System.out.print(i+"|");
-        }
+
+    /**
+     *
+     * @return retourne un pointeur vers Vect
+     */
+    public int[] getVect() {
+        return Vect;
     }
-   
-    /** Donner la taille du vecteur 
+
+    /**
      *
-     * @param Vect vecteur d'entiers 
-     * @return
+     * @return le minimum dans Vect
      */
-    public int length(int Vect[])
-   {   int l=0; 
-    for (int i: Vect)
-   { 
-       l++;
-   } 
-      return l;
-   }  
-   
-    /** DOnner le min et max d'un vecteur 
+    public int getMin() {
+        return min;
+    }
+
+    /**
      *
-     * @param Vect veteur d'entier 
+     * @return le maximum du Vect
      */
-    public void MaxMin (int Vect[] ){
+    public int getMax() {
+        return max;
+    }
+
+    /**
+     *
+     * @return la taille du Vect
+     */
+    public int getL() {
+        return l;
+    }
+  
+    /**
+     * calculer le min et le max du vect 
+     * exemple : Vect ={1,2,3,47,7}
+     * min=1; max=47
+     */
+    public void MaxMin ( ){
       
-        int max=Vect[0];
-        int min=Vect[0];
+         max=Vect[0];
+         min=Vect[0];
        
        for( int i: Vect)
        {
@@ -64,12 +82,12 @@ public class VectorHelper {
        System.out.println("Max= "+max+", Min="+min+" \n");
    }
    
-    /** inverser les éléments d'un vecteur 
-     *
-     * @param Vect vecteur d'entiers 
-     * @param l la taille du vecteur Vect
+    /**
+     * inverser les éléments du Vect
+     * exemple: vect={4,8,7,46,93,3,1}
+     *          resultat : vect={1,3,93,46,7,8,4}
      */
-    public void inverser(int []Vect,int l)
+    public void inverser()
    {
        int inf,sup,x;
        inf=0; sup=l-1;
@@ -85,15 +103,18 @@ public class VectorHelper {
      
    }
    
-    /** Faire la somme des deux vecteurs v1 et v2
+    /**
      *
-     * @param V1 un vecteur d'entiers 
+     * @param V1 un vecteur d'entiers
      * @param V2 un vecteur d'entiers 
-     * @param l1 taille du v1
-     * @param l2 taille du v1
-     * @throws diffrenceInLengthException lancer une exception si les deux vecteurs ont des taille différentes 
+     * @param l1 la taille du V1
+     * @param l2 la taille du V2
+     * @throws diffrenceInLengthException
+     * exemple : V1={1,2,-1,7} V2={0,5,6,4}
+     *           l1=l2= 4
+     *           resultat: V1={1,7,5,11} V2={0,5,6,4}
      */
-    public void Somme2Vect(int V1[], int V2[], int l1,int l2) throws diffrenceInLengthException{
+    public  static void Somme2Vect( int V1[],int V2[],int l1,int l2 ) throws diffrenceInLengthException{
        
        if(l1!=l2)
            throw new diffrenceInLengthException(" ERROR! les deux vecteurs n'ont pas la meme longueur");
@@ -107,23 +128,23 @@ public class VectorHelper {
            
    }
    
-    /** trier les éléments du vecteur par la méthode "tri bulle"
-     *
-     * @param V un vecteur d'entiers
-     * @param l la taille du vecteur v
+    /**
+     * trier les éléments du Vect
+     * exemple : vect={7,8,-1,-2,6,7,3}
+     *           resultat: vect{-2,-1,3,6,7,7,8}
      */
-    public void Trie_Bulle( int V[],int l)
+    public void Trie( )
    {       boolean verif=true;
            int m=l;
    while (verif || m>2)
    {
         verif =false;
         for (int i=0;i<m-1;i++)
-            {if (V[i]>V[i+1] || V[i]== V[i+1])
+            {if (Vect[i]>Vect[i+1] || Vect[i]== Vect[i+1])
                 {
-                 int sauve =V[i];
-                 V[i]=V[i+1] ;
-                 V[i+1]=sauve;
+                 int sauve =Vect[i];
+                 Vect[i]=Vect[i+1] ;
+                 Vect[i+1]=sauve;
                  verif=true;
                 
            
@@ -133,19 +154,19 @@ public class VectorHelper {
     }
    }
    
-    /** Multiplier les éléments du vecteur par l'entier l
-     *
-     * @param v vevteur d'entiers 
-     * @param l entier 
+    /**
+     * multiplier les éléments du Vect par n
+     * @param n un entier 
+     * exemple: Vect={1,5,8,9} n=2
+     *          Resultat : Vect={2,10,16,18}
      */
-    public void Vector_produit(int v[], int l)
+    public void Vector_produit( int n)
    { int j=0;
-    for (int i:v)
+    for (int i:Vect)
     {
-        v[j]=l*i; j++;
+        Vect[j]=n*i; j++;
     }
    
    } 
-    
     
 }
