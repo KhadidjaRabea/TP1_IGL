e16c27a44f6a644b0426567496a816feec761ed4
Khadidja <Khadidja@DESKTOP-1U2D94O>
2017-10-25 13:36:20 +0200

    version_finale.1.4

--- src/tp1_igl/TP1_IGL.java
+++ src/tp1_igl/TP1_IGL.java
@@ -5,6 +5,9 @@
  */
 package tp1_igl;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Khadidja
@@ -13,7 +16,55 @@ public class TP1_IGL {
 
    
     public static void main(String[] args) {
-     
+        
+        int [] v1={4,1,2,3};
+        int [] v2={0,7,-1,2};
+        
+        VectorHelper vect1=new VectorHelper(v1);
+        VectorHelper vect2=new VectorHelper(v2);
+         
+        /****** Afficher le vecteur **************/
+        
+        System.out.println(" **** La fonction affiche ****");
+        vect1.Affich();
+        
+        /******* trier les éléments du vecteur ****/
+        
+        System.out.println(" **** La fonction Trie ****");
+        vect1.Trie();
+        vect1.Affich();
+        
+        /****** inverser les éléments du vecteur ****/
+      
+        System.out.println(" **** La fonction inverser ****");
+        vect1.inverser();
+        vect1.Affich();
+        
+        /********* affiher le min et le max ******/
+        
+        System.out.println("**** La fonction MinMax *** ");
+        vect1.MaxMin();
+       
+        /******** Somme de deux vecteurs *******/
+        
+        System.out.println("***** la fonction somme2Vect*****");
+        vect1.Affich();
+        vect2.Affich();
+        try {
+            VectorHelper.Somme2Vect(vect1.getVect(), vect2.getVect(), vect1.getL(), vect2.getL());
+            vect1.Affich();
+        } catch (diffrenceInLengthException ex) {
+            Logger.getLogger(TP1_IGL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+         
+        
+        /********* multiplier les éléments du vecteurs par un entier n *****/
+        
+        System.out.println("***** la fonction Vctor_produit*****");
+        vect1.Vector_produit(2);
+        vect1.Affich();
+        
+        
     }
     
 }
